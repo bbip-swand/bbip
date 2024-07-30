325ab19630a180630eac1210577368c96cfacc19
GeonWoo Lee <2dubu.dev@gmail.com>
2024-07-30 20:56:02 +0900

    🎉 :: tada
    create project file

---
+++ BBIP/BBIP/BBIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBIPApp.swift
+//  BBIP
+//
+//  Created by 이건우 on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BBIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
