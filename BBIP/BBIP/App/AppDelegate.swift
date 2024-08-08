bcfddeb24cf956062fe21c37da6cf12a5d2964f6
GeonWoo Lee <2dubu.dev@gmail.com>
2024-08-08 18:53:18 +0900

    🔧 :: 폴더 구조 재설정
    Clean Architecture 적용

---
+++ BBIP/BBIP/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  BBIP
+//
+//  Created by 이건우 on 8/8/24.
+//
+
+import Foundation
+
+class AppDelegate {
+    
+}
---
+++ BBIP/BBIP/Domain/UseCase/Example.swift
@@ -0,0 +1,8 @@
+//
+//  Example.swift
+//  BBIP
+//
+//  Created by 이건우 on 8/8/24.
+//
+
+import Foundation
