2f9da5a9aa01195e66163aea695f62cbfe4fd2e7
GeonWoo Lee <2dubu.dev@gmail.com>
2024-08-11 00:08:42 +0900

    ✨ :: Onboarding Animation
    modify animation part

--- BBIP/BBIP/Feature/Onboarding/OnboardingView.swift
+++ BBIP/BBIP/Feature/Onboarding/OnboardingView.swift
@@ -9,41 +9,29 @@ import SwiftUI
 
 struct OnboardingView: View {
     @State private var selection = 0
-    private let imageName = ["a", "b", "c", "d"]
+    private let onboardingData: [OnboardingData]
+    
+    init() {
+        self.onboardingData = OnboardingView.generate()
+    }
     
     var body: some View {
         ZStack {
             TabView(selection: $selection) {
                 
                 // 추후 이미지로 변경
-                RoundedRectangle(cornerRadius: 16)
-                    .foregroundStyle(.gray3)
-                    .padding(.horizontal, 58)
-                    .padding(.top, 230)
-                    .tag(0)
-                
-                RoundedRectangle(cornerRadius: 16)
-                    .foregroundStyle(.gray4)
-                    .padding(.horizontal, 58)
-                    .padding(.top, 230)
-                    .tag(1)
-                
-                RoundedRectangle(cornerRadius: 16)
-                    .foregroundStyle(.gray5)
-                    .padding(.horizontal, 58)
-                    .padding(.top, 230)
-                    .tag(2)
-                
-                RoundedRectangle(cornerRadius: 16)
-                    .foregroundStyle(.gray6)
-                    .padding(.horizontal, 58)
-                    .padding(.top, 230)
-                    .tag(3)
+                ForEach(0..<onboardingData.count, id: \.self) { index in
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundStyle(Color.gray4)
+                        .padding(.horizontal, 58)
+                        .padding(.top, 230)
+                        .tag(index)
+                }
             }
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             
             HStack(spacing: 8) {
-                ForEach(0..<imageName.count, id: \.self) { index in
+                ForEach(0..<onboardingData.count, id: \.self) { index in
                     Circle()
                         .fill(selection == index ? Color.gray7 : Color.gray3)
                         .frame(width: 8, height: 8)
@@ -53,9 +41,9 @@ struct OnboardingView: View {
             .padding(.top, 52)
             
             VStack(spacing: 5) {
-                Text("한눈에 보이는 학습,")
+                Text(onboardingData[selection].firstTitle)
                     .font(.bbip(family: .Regular, size: 28))
-                Text("함께하는 도전")
+                Text(onboardingData[selection].secondTitle)
                     .font(.bbip(.title1_sb28))
             }
             .frame(maxHeight: .infinity, alignment: .top)
@@ -71,11 +59,11 @@ struct OnboardingView: View {
                     )
                 )
                 .frame(maxHeight: .infinity, alignment: .bottom)
-                .edgesIgnoringSafeArea(.bottom)
+                .ignoresSafeArea(edges: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
             
             MainButton(text: "다음") {
                 withAnimation {
-                    if selection < imageName.count - 1 {
+                    if selection < onboardingData.count - 1 {
                         selection += 1
                     } else {
                         selection = 0
@@ -86,6 +74,23 @@ struct OnboardingView: View {
             .frame(maxHeight: .infinity, alignment: .bottom)
             .padding(.bottom, 39)
         }
+    }
+}
+
+private extension OnboardingView {
+    struct OnboardingData: Hashable {
+        let firstTitle: String
+        let secondTitle: String
+        let imageName: String
+    }
+    
+    static func generate() -> [OnboardingData] {
+        return [
+            OnboardingData(firstTitle: "한눈에 보이는 학습", secondTitle: "함께하는 도전1", imageName: "a"),
+            OnboardingData(firstTitle: "두눈에 보이는 학습", secondTitle: "함께하는 도전2", imageName: "b"),
+            OnboardingData(firstTitle: "세눈에 보이는 학습", secondTitle: "함께하는 도전3", imageName: "c"),
+            OnboardingData(firstTitle: "네눈에 보이는 학습", secondTitle: "함께하는 도전4", imageName: "d")
+        ]
     }
 }
 
