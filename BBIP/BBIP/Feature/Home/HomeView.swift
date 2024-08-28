823099f2dbfebec549910617755913e478aa5c3b
GeonWoo Lee <2dubu.dev@gmail.com>
2024-08-28 17:15:10 +0900

    🔧 :: login, home
    remove example file

---
+++ BBIP/BBIP/Feature/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BBIP
+//
+//  Created by 이건우 on 8/28/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ BBIP/BBIP/Feature/SignIn/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  BBIP
+//
+//  Created by 이건우 on 8/28/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login")
+    }
+}
+
+#Preview {
+    LoginView()
+}
