26e8f3b2b51fcf006da90c392a4e843a1e91b63a
GeonWoo Lee <2dubu.dev@gmail.com>
2024-08-31 00:52:17 +0900

    ✨ :: Add view files
    with innerView folder

---
+++ BBIP/BBIP/Feature/CreateStudy/SISInnerView/SISDescriptionView.swift
@@ -0,0 +1,18 @@
+//
+//  SISDescriptionView.swift
+//  BBIP
+//
+//  Created by 이건우 on 8/31/24.
+//
+
+import SwiftUI
+
+struct SISDescriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SISDescriptionView()
+}
