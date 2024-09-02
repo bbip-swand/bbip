5697408d2d23b40214c15045909149a429578638
GeonWoo Lee <2dubu.dev@gmail.com>
2024-09-02 15:56:19 +0900

    🔧 :: ImagePicker

--- BBIP/BBIP/Feature/UserInfoSetup/UISInnerView/UISProfileView.swift
+++ BBIP/BBIP/Feature/UserInfoSetup/UISInnerView/UISProfileView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import PhotosUI
 import SwiftUIIntrospect
 
 struct UISProfileView: View {
@@ -98,48 +97,5 @@ private struct SetNicknameView: View {
         let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
         viewModel.isNameValid = predicate.evaluate(with: name)
         viewModel.canGoNext[2] = predicate.evaluate(with: name)
-    }
-}
-
-// ImagePicker
-struct ImagePicker: UIViewControllerRepresentable {
-    @Binding var image: UIImage?
-    
-    func makeUIViewController(context: Context) -> PHPickerViewController {
-        var config = PHPickerConfiguration()
-        config.filter = .images
-        config.selectionLimit = 1
-        
-        let picker = PHPickerViewController(configuration: config)
-        picker.delegate = context.coordinator
-        return picker
-    }
-    
-    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    class Coordinator: NSObject, PHPickerViewControllerDelegate {
-        let parent: ImagePicker
-        
-        init(_ parent: ImagePicker) {
-            self.parent = parent
-        }
-        
-        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            picker.dismiss(animated: true)
-            
-            guard let provider = results.first?.itemProvider else { return }
-            
-            if provider.canLoadObject(ofClass: UIImage.self) {
-                provider.loadObject(ofClass: UIImage.self) { image, _ in
-                    DispatchQueue.main.async {
-                        self.parent.image = image as? UIImage
-                    }
-                }
-            }
-        }
     }
 }
