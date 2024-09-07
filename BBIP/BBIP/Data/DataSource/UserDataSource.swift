56b37d09787218e4628139b7d694339b723f955d
GeonWoo Lee <2dubu.dev@gmail.com>
2024-09-07 21:52:08 +0900

    ✨ :: User DS, Repo

--- BBIP/BBIP/Data/DataSource/UserDataSource.swift
+++ BBIP/BBIP/Data/DataSource/UserDataSource.swift
@@ -13,14 +13,69 @@ import CombineMoya
 final class UserDataSource {
     private let provider = MoyaProvider<UserAPI>()
     
-    func requestSignIn(signUpReqDTO: SignUpRequestDTO) -> AnyPublisher<Bool, Error> {
+    func requestSignUp(signUpReqDTO: SignUpRequestDTO) -> AnyPublisher<Bool, Error> {
         provider.requestPublisher(.signUp(dto: signUpReqDTO))
             .tryMap { response in
                 guard (200...299).contains(response.statusCode) else {
                     throw NSError(
-                        domain: "SignIn Error",
+                        domain: "SignUp Error",
                         code: response.statusCode,
-                        userInfo: [NSLocalizedDescriptionKey: "[UserDataSource] requestSignIn() failed with status code \(response.statusCode)"]
+                        userInfo: [NSLocalizedDescriptionKey: "[UserDataSource] requestSignUp() failed with status code \(response.statusCode)"]
+                    )
+                }
+                return true
+            }
+            .mapError { error in
+                return error
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func requestResign() -> AnyPublisher<Bool, Error> {
+        provider.requestPublisher(.resign)
+            .tryMap { response in
+                guard (200...299).contains(response.statusCode) else {
+                    throw NSError(
+                        domain: "Resign Error",
+                        code: response.statusCode,
+                        userInfo: [NSLocalizedDescriptionKey: "[UserDataSource] requestResign() failed with status code \(response.statusCode)"]
+                    )
+                }
+                return true
+            }
+            .mapError { error in
+                return error
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func createUserInfo(userInfoDTO: UserInfoDTO) -> AnyPublisher<Bool, Error> {
+        provider.requestPublisher(.info(dto: userInfoDTO))
+            .tryMap { response in
+                guard (200...299).contains(response.statusCode) else {
+                    throw NSError(
+                        domain: "Create UIS Error",
+                        code: response.statusCode,
+                        userInfo: [NSLocalizedDescriptionKey: "[UserDataSource] createUserInfo() failed with status code \(response.statusCode)"]
+                    )
+                }
+                return true
+            }
+            .mapError { error in
+                return error
+            }
+            .eraseToAnyPublisher()
+            
+    }
+    
+    func updateUserInfo(userInfoDTO: UserInfoDTO) -> AnyPublisher<Bool, Error> {
+        provider.requestPublisher(.updateInfo(dto: userInfoDTO))
+            .tryMap { response in
+                guard (200...299).contains(response.statusCode) else {
+                    throw NSError(
+                        domain: "Update UIS Error",
+                        code: response.statusCode,
+                        userInfo: [NSLocalizedDescriptionKey: "[UserDataSource] updateUserInfo() failed with status code \(response.statusCode)"]
                     )
                 }
                 return true
---
+++ BBIP/BBIP/Data/Repository/UserRepository.swift
@@ -0,0 +1,46 @@
+//
+//  UserRepository.swift
+//  BBIP
+//
+//  Created by 이건우 on 9/7/24.
+//
+
+import Foundation
+import Combine
+
+protocol UserRepositoryProtocol {
+    func signUp(signUpDTO: SignUpRequestDTO) -> AnyPublisher<Bool, Error>
+    func resign() -> AnyPublisher<Bool, Error>
+    func createUserInfo(userInfoDTO: UserInfoDTO) -> AnyPublisher<Bool, Error>
+    func updateUserInfo(userInfoDTO: UserInfoDTO) -> AnyPublisher<Bool, Error>
+}
+
+final class UserRepository: UserRepositoryProtocol {
+    private let dataSource: UserDataSource
+    // mapper 아직 필요 없음
+
+    init(dataSource: UserDataSource) {
+        self.dataSource = dataSource
+    }
+
+    func signUp(signUpDTO: SignUpRequestDTO) -> AnyPublisher<Bool, Error> {
+        dataSource.requestSignUp(signUpReqDTO: signUpDTO)
+            .eraseToAnyPublisher()
+    }
+    
+    func resign() -> AnyPublisher<Bool, Error> {
+        dataSource.requestResign()
+            .eraseToAnyPublisher()
+    }
+
+    func createUserInfo(userInfoDTO: UserInfoDTO) -> AnyPublisher<Bool, Error> {
+        dataSource.createUserInfo(userInfoDTO: userInfoDTO)
+            .eraseToAnyPublisher()
+    }
+
+    func updateUserInfo(userInfoDTO: UserInfoDTO) -> AnyPublisher<Bool, Error> {
+        dataSource.updateUserInfo(userInfoDTO: userInfoDTO)
+            .eraseToAnyPublisher()
+    }
+}
+
