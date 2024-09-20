585ad1d3bb6e5bacb18d6068cea0166e987108c8
Geonwoo Lee <2dubu.dev@gmail.com>
2024-09-20 17:18:02 +0900

    ✨ :: CreateStudyUseCase

--- BBIP/BBIP/Domain/UseCase/Study/CreateStudyUseCase.swift
+++ BBIP/BBIP/Domain/UseCase/Study/CreateStudyUseCase.swift
@@ -5,4 +5,21 @@
 //  Created by 이건우 on 9/20/24.
 //
 
-import Foundation
+import Combine
+
+protocol CreateStudyUseCaseProtocol {
+    func excute(studyInfoVO: StudyInfoVO) -> AnyPublisher<Bool, Error>
+}
+
+final class CreateStudyUseCase: CreateStudyUseCaseProtocol {
+    private let repository: StudyRepositoryProtocol
+    
+    init(repository: StudyRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    func excute(studyInfoVO: StudyInfoVO) -> AnyPublisher<Bool, Error> {
+        repository.createStudy(vo: studyInfoVO)
+            .eraseToAnyPublisher()
+    }
+}
