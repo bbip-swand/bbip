9a9ebf8101a511d06739e58de13439f53350e301
Geonwoo Lee <2dubu.dev@gmail.com>
2024-09-20 16:40:46 +0900

    ✨ :: StudyAPI with DTO

--- BBIP/BBIP/Data/DataSource/API/StudyAPI.swift
+++ BBIP/BBIP/Data/DataSource/API/StudyAPI.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+import Moya
+
+enum StudyAPI {
+    case getThisWeekStudy
+    case getStudyInfo(studyId: String)      // param
+    case getInviteInfo(inviteCode: String)  // param
+    case createStudy(dto: StudyInfoDTO)
+    case joinStudy(studyId: String)         // param
+}
+
+extension StudyAPI: BaseAPI {
+    var path: String {
+        switch self {
+        case .getThisWeekStudy:
+            return "/study/this-week"
+        case .getStudyInfo:
+            return "/study"
+        case .getInviteInfo:
+            return "/study/invite-info"
+        case .createStudy:
+            return "/study/create"
+        case .joinStudy:
+            return "/study/join"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getThisWeekStudy:
+            return .get
+        case .getStudyInfo:
+            return .get
+        case .getInviteInfo:
+            return .get
+        case .createStudy:
+            return .post
+        case .joinStudy:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getThisWeekStudy:
+            return .requestPlain
+            
+        case .getStudyInfo(let studyId):
+            let param = ["studyId" : studyId]
+            return .requestParameters(parameters: param, encoding: URLEncoding.queryString)
+            
+        case .getInviteInfo(let inviteCode):
+            let param = ["inviteCode" : inviteCode]
+            return .requestParameters(parameters: param, encoding: URLEncoding.queryString)
+            
+        case .createStudy(let dto):
+            return .requestJSONEncodable(dto)
+            
+        case .joinStudy(let studyId):
+            let param = ["studyId" : studyId]
+            return .requestParameters(parameters: param, encoding: URLEncoding.queryString)
+        }
+    }
+}
