a8b136e959427ecde608cdd7be7aefc778ba6097
Geonwoo Lee <2dubu.dev@gmail.com>
2024-09-21 17:35:54 +0900

    ♻️ :: AWSS3 Logic

--- BBIP/BBIP/Feature/UserInfoSetup/UserInfoSetupViewModel.swift
+++ BBIP/BBIP/Feature/UserInfoSetup/UserInfoSetupViewModel.swift
@@ -74,80 +74,55 @@ class UserInfoSetupViewModel: ObservableObject {
     
     func createUserInfo() {
         isLoading = true
+
+        let uploadPublisher: AnyPublisher<String, Error>
         
+        // 사용자가 이미지를 선택했다면 업로드 후 S3 bucket URL, 선택하지 않았다면 빈 문자열
         if let selectedImage = selectedImage {
-            // selectedImage가 nil이 아닐 경우에만 업로드 실행 (사용자가 사진 선택함)
-            AWSS3Manager.shared.upload(image: selectedImage)
-                .receive(on: DispatchQueue.main)
-                .flatMap { [weak self] uploadedImageUrl -> AnyPublisher<Bool, Error> in
-                    guard let self = self else {
-                        return Fail(error: URLError(.unknown)).eraseToAnyPublisher()
-                    }
-                    
-                    // UserInfoVO 객체 생성
-                    let vo = UserInfoVO(
-                        selectedArea: self.selectedArea,
-                        selectedInterestIndex: self.selectedInterestIndex,
-                        userName: self.userName,
-                        profileImageUrl: uploadedImageUrl,
-                        birthYear: self.combinedYear,
-                        selectedJobIndex: self.selectedJobIndex
-                    )
-                    
-                    // 회원 정보를 생성
-                    return self.createUserInfoUseCase.execute(userInfoVO: vo)
-                }
-                .receive(on: DispatchQueue.main)
-                .sink { [weak self] completion in
-                    guard let self = self else { return }
-                    switch completion {
-                    case .finished: break
-                    case .failure(let error):
-                        self.isLoading = false
-                        print("회원가입 실패: \(error.localizedDescription)")
-                    }
-                } receiveValue: { isSuccess in
-                    if isSuccess {
-                        self.isLoading = false
-                        self.showCompleteView = true
-                        UserDefaultsManager.shared.setIsLoggedIn(true)
-                    } else {
-                        fatalError("회원가입 문제 발생")
-                    }
-                }
-                .store(in: &cancellables)
+            uploadPublisher = AWSS3Manager.shared.upload(image: selectedImage)
         } else {
-            // selectedImage가 nil일 경우 빈 데이터 보내기 (사용자가 사진 선택 안함)
-            let vo = UserInfoVO(
-                selectedArea: self.selectedArea,
-                selectedInterestIndex: self.selectedInterestIndex,
-                userName: self.userName,
-                profileImageUrl: "",
-                birthYear: self.combinedYear,
-                selectedJobIndex: self.selectedJobIndex
-            )
-            
-            // 회원 정보를 생성
-            self.createUserInfoUseCase.execute(userInfoVO: vo)
-                .receive(on: DispatchQueue.main)
-                .sink { [weak self] completion in
-                    guard let self = self else { return }
-                    switch completion {
-                    case .finished: break
-                    case .failure(let error):
-                        self.isLoading = false
-                        print("회원가입 실패: \(error.localizedDescription)")
-                    }
-                } receiveValue: { isSuccess in
-                    if isSuccess {
-                        self.isLoading = false
-                        self.showCompleteView = true
-                        UserDefaultsManager.shared.setIsLoggedIn(true)
-                    } else {
-                        fatalError("회원가입 문제 발생")
-                    }
-                }
-                .store(in: &cancellables)
+            uploadPublisher = Just("")
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
         }
+        
+        uploadPublisher
+            .receive(on: DispatchQueue.main)
+            .flatMap { [weak self] uploadedImageUrl -> AnyPublisher<Bool, Error> in
+                guard let self = self else {
+                    return Fail(error: URLError(.unknown)).eraseToAnyPublisher()
+                }
+
+                let vo = UserInfoVO(
+                    selectedArea: self.selectedArea,
+                    selectedInterestIndex: self.selectedInterestIndex,
+                    userName: self.userName,
+                    profileImageUrl: uploadedImageUrl,
+                    birthYear: self.combinedYear,
+                    selectedJobIndex: self.selectedJobIndex
+                )
+
+                return self.createUserInfoUseCase.execute(userInfoVO: vo)
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                guard let self = self else { return }
+                switch completion {
+                case .finished: break
+                case .failure(let error):
+                    self.isLoading = false
+                    print("회원가입 실패: \(error.localizedDescription)")
+                }
+            } receiveValue: { [weak self] isSuccess in
+                guard let self = self else { return }
+                self.isLoading = false
+                if isSuccess {
+                    self.showCompleteView = true
+                    UserDefaultsManager.shared.setIsLoggedIn(true)
+                } else {
+                    fatalError("회원가입 문제 발생")
+                }
+            }
+            .store(in: &cancellables)
     }
 }
