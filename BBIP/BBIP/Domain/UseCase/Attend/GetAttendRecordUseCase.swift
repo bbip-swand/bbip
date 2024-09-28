a50c19776fa023b04af61260d65ec72245cacfe8
whdpfls <lmkm909@gmail.com>
2024-09-28 14:11:20 +0900

    GetAttendRecord UseCase

---
+++ BBIP/BBIP/Domain/UseCase/Attend/GetAttendRecordUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  GetAttendRecordUseCase.swift
+//  BBIP
+//
+//  Created by 조예린 on 9/28/24.
+//
+
+import Foundation
+import Combine
+
+protocol GetAttendRecordUseCaseProtocol{
+    func execute(studyId: String) -> AnyPublisher<getAttendRecordVO, Error>
+}
+
+final class GetAttendRecordUseCase: GetAttendRecordUseCaseProtocol{
+    private let repository : AttendRepository
+    
+    init (repository: AttendRepository){
+        self.repository = repository
+    }
+    
+    func execute(studyId: String) -> AnyPublisher<getAttendRecordVO, any Error> {
+        return repository.getAttendRecord(studyId: studyId)
+            .eraseToAnyPublisher()
+    }
+}
