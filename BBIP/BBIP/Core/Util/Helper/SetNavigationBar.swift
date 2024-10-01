905e6fe838bb08e242780dab13db900befd35e9d
Geonwoo Lee <2dubu.dev@gmail.com>
2024-10-01 19:23:20 +0900

    🔇 :: Remove logs

--- BBIP/BBIP/Core/Util/Helper/SetNavigationBar.swift
+++ BBIP/BBIP/Core/Util/Helper/SetNavigationBar.swift
@@ -11,7 +11,6 @@ import SwiftUI
 extension View {
     /// Navigation title이 존재하는 뷰에서 전반적으로 사용될 appearance custom 함수
     func setNavigationBarAppearance(forDarkView: Bool = false) {
-        print("setNavigationBarAppearance")
         UINavigationBar.appearance().titleTextAttributes = [
             .font: UIFont(name: "WantedSans-Medium", size: 20)!,
             .foregroundColor: forDarkView ? UIColor.white : UIColor(hexCode: "1F1F1F")
