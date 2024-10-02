dbd9c5ee24baec65af0b7f08ccfd3c76d5234d3b
whdpfls <lmkm909@gmail.com>
2024-10-02 22:55:30 +0900

    파일 옮기기

---
+++ BBIP/BBIP/Domain/UseCase/Study/GetFullStudyInfoUseCase.swift
@@ -0,0 +1,25 @@
+//
+//  GetFullStudyInfoUseCase.swift
+//  BBIP
+//
+//  Created by 이건우 on 9/27/24.
+//
+
+import Combine
+
+protocol GetFullStudyInfoUseCaseProtocol {
+    func excute(studyId: String) -> AnyPublisher<FullStudyInfoVO, Error>
+}
+
+final class GetFullStudyInfoUseCase: GetFullStudyInfoUseCaseProtocol {
+    private let repository: StudyRepository
+    
+    init(repository: StudyRepository) {
+        self.repository = repository
+    }
+    
+    func excute(studyId: String) -> AnyPublisher<FullStudyInfoVO, Error> {
+        repository.getFullStudyInfo(studyId: studyId)
+            .eraseToAnyPublisher()
+    }
+}
