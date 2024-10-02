21c877e2ec0afa3a4a1288d0e80c3daa8395ae7e
Geonwoo Lee <2dubu.dev@gmail.com>
2024-10-03 04:21:54 +0900

    ♻️ :: ArchiveView
    ui issue

---
+++ BBIP/BBIP/Core/Util/Helper/SafeAreaInsets.swift
@@ -0,0 +1,26 @@
+//
+//  SafeAreaInsets.swift
+//  BBIP
+//
+//  Created by 이건우 on 10/3/24.
+//
+
+import SwiftUI
+
+private struct SafeAreaInsetsKey: EnvironmentKey {
+    static var defaultValue: EdgeInsets {
+        (UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.safeAreaInsets ?? .zero).insets
+    }
+}
+
+extension EnvironmentValues {
+    var safeAreaInsets: EdgeInsets {
+        self[SafeAreaInsetsKey.self]
+    }
+}
+
+private extension UIEdgeInsets {
+    var insets: EdgeInsets {
+        EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
+    }
+}
